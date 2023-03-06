1869b30223c929d4cf4861ac5187be20e339f17a
Mosesbwire <mosesdev92@gmail.com>
2023-03-06 13:18:12 +0000

    complete task 5 - 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_three = word[:3]
+word_last_two = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_three}")
+print(f"Last 2 letters: {word_last_two}")
+print(f"Middle word: {middle_word}")
