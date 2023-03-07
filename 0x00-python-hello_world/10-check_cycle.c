0b5e6f8c011b4958b3ae141fd09bccab696a6254
Mosesbwire <mosesdev92@gmail.com>
2023-03-07 05:57:30 +0000

    add semicolon to last member of struct in lists.h and remove undeclared variables from source code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *list)
 	listint_t *slow_ptr = list;
 	listint_t *fast_ptr = list;
 
-	while (fast != NULL && fast->next != NULL)
+	while (fast_ptr != NULL && fast_ptr->next != NULL)
 	{
 		slow_ptr = slow_ptr->next;
 		fast_ptr = fast_ptr->next->next;
