088b8484405db30698e0dbad27013b47406166ab
Mosesbwire <mosesdev92@gmail.com>
2023-03-07 21:30:03 +0000

    Fix compile time error

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,14 +15,14 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new->n = number;
-	new->next = NULL;
+	new_node->n = number;
+	new_node->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-	tracker = current->next->next
+	tracker = current->next->next;
 	if (current->n > current->next->n)
 	{
 		while (current->n > number)
