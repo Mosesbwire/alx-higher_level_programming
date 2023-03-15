da76f4de39f0a6436c89754716cd618282e17417
Mosesbwire <mosesdev92@gmail.com>
2023-03-15 21:16:01 +0000

    Fix. To fit requiremnt of  max line

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    answer = list(map(lambda x: x * number, my_list))
-    return answer
+    return list(map(lambda x: x * number, my_list))
+    
