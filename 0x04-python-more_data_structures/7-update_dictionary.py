81e6f01a7547e04ec861f662533bc1121cc14d86
Mosesbwire <mosesdev92@gmail.com>
2023-03-15 12:08:58 +0000

    Complete task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    if (not isinstance(a_dictionary, dict)):
+        return
+
+    a_dictionary[key] = value
+    return a_dictionary
+
+
+if __name__ == "__main__":
+    update_dictionary({}, 'key', 'value')
