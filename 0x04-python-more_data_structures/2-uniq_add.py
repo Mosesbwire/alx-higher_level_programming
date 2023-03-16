521c84dc889b1b5cec216e85fcb8c1c00eb17858
Mosesbwire <mosesdev92@gmail.com>
2023-03-16 15:10:14 +0000

    check len of list if 0 return 0

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    if (not isinstance(my_list, list) or len(my_list) == 0):
+    if (not isinstance(my_list, list)):
         return
     result = 0
+
+    if len(my_list) == 0:
+        return 0
+
     for x in set(my_list):
         result += x
     return result
