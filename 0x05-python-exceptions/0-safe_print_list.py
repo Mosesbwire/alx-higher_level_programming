f6182f1280674cb2b52c4cf3d273ea1c795493ed
Mosesbwire <mosesdev92@gmail.com>
2023-03-27 10:01:20 +0000

    Fix coding style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x = 0):
+def safe_print_list(my_list=[], x=0):
     elements_printed = 0
 
     try:
@@ -10,5 +10,6 @@ def safe_print_list(my_list=[], x = 0):
 
     except IndexError:
         print("")
-
+    
+    print("")
     return elements_printed
