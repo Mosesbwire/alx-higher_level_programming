fadac88401f496698375bc50b123e022ba8416a6
Mosesbwire <mosesdev92@gmail.com>
2023-03-27 10:40:15 +0000

    check if iterable passed in is a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    elements_printed = 0
+    count = 0
+
+    try:
+        if (not isinstance(my_list, list)):
+            raise TypeError
+
+    except TypeError:
+        print()
+        return count
 
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-            elements_printed += 1
-
+            count += 1
     except IndexError:
         print()
     except TypeError:
         print()
     print()
-    return elements_printed
+    return count
