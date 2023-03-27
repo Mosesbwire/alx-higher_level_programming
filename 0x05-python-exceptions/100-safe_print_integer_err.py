b47735402d3e95983144f63022b41bc9a2299fba
Mosesbwire <mosesdev92@gmail.com>
2023-03-27 20:33:50 +0000

    Catch ValueError exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,11 +3,10 @@ import sys
 
 
 def safe_print_integer_err(value):
-    control = 1
+
     try:
-        control + value
         print("{:d}".format(value))
         return True
-    except TypeError as e:
+    except ValueError as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
