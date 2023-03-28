8dee7e2b6869685aca12e6ed4e7fd13d844ded15
Mosesbwire <mosesdev92@gmail.com>
2023-03-28 05:55:13 +0000

    Remove unnecessary print statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,8 +16,8 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             count += 1
     except IndexError:
-        print()
+        
     except TypeError:
-        print()
+        
     print()
     return count
