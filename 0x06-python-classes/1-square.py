f02de060230b217a95ed80978dbc144bff8e0104
Mosesbwire <mosesdev92@gmail.com>
2023-03-28 11:13:51 +0000

    Complete task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines a class for a square
+"""
+
+
+class Square:
+    """ Defines a square """
+
+    def __init__(self, size):
+        """
+        This is the initializer method
+        Args:
+            size: Lenght of the square
+        """
+        self.__size = size
