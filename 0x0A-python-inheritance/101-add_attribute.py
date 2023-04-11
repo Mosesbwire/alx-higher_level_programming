7c09d7532d0c5bfc7be251c763bc80396df8a0bc
Mosesbwire <mosesdev92@gmail.com>
2023-04-11 16:06:47 +0000

    Task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module 101-add_attribute
+
+sets attribute of an object
+
+"""
+
+
+def add_attribute(obj, key, value):
+    """dynamically set the attribute of an object
+    Args:
+        obj (object): object to set value
+        key (str): name of the attribute
+        value (obj): value for the attribute
+    """
+    if (type(obj) in (int, float, complex, str, tuple, frozenset)):
+        raise TypeError("can\'t add new attribute")
+    setattr(obj, key, value)
