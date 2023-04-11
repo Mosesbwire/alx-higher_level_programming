cdf9ca0b53813b42d962f29b03ea51df483ae2ef
Mosesbwire <mosesdev92@gmail.com>
2023-04-11 15:00:42 +0000

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module 11-square
+Represents a square
+
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """represents a square"""
+
+    def __init__(self, size):
+        """ initializes square object """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """calculates area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
