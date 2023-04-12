d254803052f7d04323ed201403ba5b5c53910dc4
Mosesbwire <mosesdev92@gmail.com>
2023-04-12 21:47:39 +0000

    Fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -15,22 +15,7 @@ class Student:
             first_name (str): first name
             last_name (str): last name
             age (int): age
-        Raises:
-            TypeError: if first or last name is not string or age is not int
-            ValueError: if age is < 0
         """
-
-        if (type(first_name) is not str):
-            raise TypeError("first_name must be a string")
-        if (type(last_name) is not str):
-            raise TypeError("last_name must be a string")
-        if (len(first_name) == 0 or len(last_name) == 0):
-            raise TypeError("first_name or last name cannot be empty string")
-
-        if (type(age) is not int):
-            raise TypeError("age must be an int")
-        if (age < 0):
-            raise ValueError("age cannot be less than 0")
 
         self.first_name = first_name
         self.last_name = last_name
@@ -38,7 +23,4 @@ class Student:
 
     def to_json(self):
         """return instance __dict__ with simple data types """
-        class_dict = self.__dict__
-        new_dict = {key: value for key, value in class_dict.items()
-                    if type(value) in (int, list, dict, bool, str)}
         return self.__dict__
