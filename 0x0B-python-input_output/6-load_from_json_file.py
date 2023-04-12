2414a88be58c6ca00772aeebafc341644c388f26
Mosesbwire <mosesdev92@gmail.com>
2023-04-12 15:51:28 +0000

    comment function

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,9 @@ import json
 
 
 def load_from_json_file(filename):
+    """deserializes an json string from a file
+    Args:
+        filename (str): name of file
+    """
     with open(filename, encoding="utf-8") as f:
         json.load(f)
