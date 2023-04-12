d14ebc58eb52b371591a5b1c27daf44c827a5ce3
Mosesbwire <mosesdev92@gmail.com>
2023-04-12 23:53:04 +0000

    supporting files

--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,10 +1,198 @@
 #!/usr/bin/python3
 read_file = __import__('0-read_file').read_file
 write_file = __import__('1-write_file').write_file
+append_write = __import__('2-append_write').append_write
+to_json_string = __import__('3-to_json_string').to_json_string
+from_json_string = __import__('4-from_json_string').from_json_string
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+MyClass = __import__('8-my_class').MyClass
+MyClass2 = __import__('8-my_class_2').MyClass2
+class_to_json = __import__('8-class_to_json').class_to_json
+Student = __import__('9-student').Student
+Student = __import__('10-student').Student
+pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
+
 
 """task 0"""
+print("task 0")
 read_file("README.md")
 
 """ task 1 """
+
+print("task 01")
 nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
 print(nb_characters)
+
+""" task 2 """
+print("task 02")
+nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
+print(nb_characters_added)
+
+""" task 3 """
+print("task 03")
+my_list = [1, 2, 3]
+s_my_list = to_json_string(my_list)
+print(s_my_list)
+print(type(s_my_list))
+
+my_dict = { 
+    'id': 12,
+    'name': "John",
+    'places': [ "San Francisco", "Tokyo" ],
+    'is_active': True,
+    'info': {
+        'age': 36,
+        'average': 3.14
+    }
+}
+s_my_dict = to_json_string(my_dict)
+print(s_my_dict)
+print(type(s_my_dict))
+
+try:
+    my_set = { 132, 3 }
+    s_my_set = to_json_string(my_set)
+    print(s_my_set)
+    print(type(s_my_set))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+""" task 4 """
+print("task 04")
+s_my_list = "[1, 2, 3]"
+my_list = from_json_string(s_my_list)
+print(my_list)
+print(type(my_list))
+
+s_my_dict = """
+{"is_active": true, "info": {"age": 36, "average": 3.14},
+"id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
+"""
+my_dict = from_json_string(s_my_dict)
+print(my_dict)
+print(type(my_dict))
+
+try:
+    s_my_dict = """
+    {"is_active": true, 12 }
+    """
+    my_dict = from_json_string(s_my_dict)
+    print(my_dict)
+    print(type(my_dict))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+""" task 5 """
+print("task 05")
+filename = "my_list.json"
+my_list = [1, 2, 3]
+save_to_json_file(my_list, filename)
+
+filename = "my_dict.json"
+my_dict = {
+    'id': 12,
+    'name': "John",
+    'places': [ "San Francisco", "Tokyo" ],
+    'is_active': True,
+    'info': {
+        'age': 36,
+        'average': 3.14
+    }
+}
+save_to_json_file(my_dict, filename)
+
+try:
+    filename = "my_set.json"
+    my_set = { 132, 3 }
+    save_to_json_file(my_set, filename)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+
+""" task 6 """
+print("-----------task 06-----------")
+
+filename = "my_list.json"
+my_list = load_from_json_file(filename)
+print(my_list)
+print(type(my_list))
+
+filename = "my_dict.json"
+my_dict = load_from_json_file(filename)
+print(my_dict)
+print(type(my_dict))
+
+try:
+    filename = "my_set_doesnt_exist.json"
+    my_set = load_from_json_file(filename)
+    print(my_set)
+    print(type(my_set))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    filename = "my_fake.json"
+    my_fake = load_from_json_file(filename)
+    print(my_fake)
+    print(type(my_fake))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+""" task 8 """
+print("-------task 08---------")
+
+m = MyClass("John")
+m.number = 89
+print(type(m))
+print(m)
+
+mj = class_to_json(m)
+print(type(mj))
+print(mj)
+
+n = MyClass2("John")
+n.win()
+print(type(n))
+print(n)
+
+nj = class_to_json(n)
+print(type(nj))
+print(nj)
+
+""" task 9 """
+print("----- task 09---------")
+students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+
+for student in students:
+    j_student = student.to_json()
+    print(type(j_student))
+    print(j_student['first_name'])
+    print(type(j_student['first_name']))
+    print(j_student['age'])
+    print(type(j_student['age']))
+
+""" task 10 """
+print("---- task 10-----")
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
+
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
+
+""" task 12 """
+print("--------- task 12 -----------------")
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+print_triangle(pascal_triangle(6))
