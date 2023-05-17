fe44252de5a342797b650f33bc7dfe13d237bee8
Mosesbwire <mosesdev92@gmail.com>
2023-05-17 12:23:27 +0000

    Fix - list all records even if name is null

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- lists all records of a given table
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NULL OR name IS NOT NULL ORDER BY score DESC;
