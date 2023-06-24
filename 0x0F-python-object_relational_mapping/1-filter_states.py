12864bc2d8cbb78b02d173535efd3b237a11bede
Mosesbwire <mosesdev92@gmail.com>
2023-06-24 11:41:50 +0000

    complete task 1 - select all data that name starts with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+Script connects to a database and gets data from specified table in ASC order
+The data is filtered according to given argument
+
+"""
+import MySQLdb
+import sys
+
+
+def fetchAll(username, password, database):
+    host = 'localhost'
+    port = 3306
+    db = MySQLdb.connect(
+        host=host,
+        port=port,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cur = db.cursor()
+    try:
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+        states = cur.fetchall()
+    except MySQLdb.Error as e:
+        print("MySQL error {}".format(str(e)))
+
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    arguments = sys.argv
+
+    if (len(arguments) != 4):
+        print("USAGE: example.py <mysql username> <mysql password>\
+                <mysql database>")
+    else:
+        username = arguments[1]
+        password = arguments[2]
+        database = arguments[3]
+
+        fetchAll(username, password, database)
